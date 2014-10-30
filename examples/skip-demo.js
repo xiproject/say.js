cd697f7b192e8666ca23c404388a35f8295f04e2
Mihir Rege <mihirrege@gmail.com>
2014-10-30 14:35:46 +0530

    Make examples OS independent

--- examples/skip-demo.js
+++ examples/skip-demo.js
@@ -1,14 +1,21 @@
 #!/usr/bin/env node
 
 'use strict';
-
+var os = require('os');
 var say = require('../');
+var voice;
+if(os.platform() === 'linux'){
+    voice = "voice_default";
+}
+else if( os.platform() === 'darwin'){
+    voice = "Vicki";
+}
 
 // no callback, fire and forget
-say.speak('voice_default', 'Hey Jude dont make it bad. When you try your best and you don\'t succed');
+say.speak(voice, 'Hey Jude dont make it bad. When you try your best and you don\'t succed');
 
 // no callback, fire and forget
-say.speak('voice_default', 'Take a sad song and make it better');
+say.speak(voice, 'Take a sad song and make it better');
 setTimeout(say.skip, 1000);
-say.speak('voice_default', 'Remember to let her into your heart');
+say.speak(voice, 'Remember to let her into your heart');
 
